5a735a4040438255966bcef49b60834f65832fbe
ki93 <hogen123@naver.com>
2019-12-11 14:53:28 +0900

    findlocation update

--- board/urls.py
+++ board/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('findmovie/location/result', views.viewMovieInfoByBranch, name="resultMovieInfoByLocation"),
     path('findmoviename/', views.findmoviename, name="findmoviename"),
     path('findbyname/', views.findbyname, name="findbyname"),
+    path('findbylocate/', views.findbylocate, name="findbylocate"),
     path('adminpage/', views.adminpage, name="adminpage"),
     path('adminpage/theater_delete/', views.theater_delete, name="theater_delete"),
 ]
